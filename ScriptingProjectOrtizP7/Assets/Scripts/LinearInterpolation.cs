3b66d0ff24010be84c308f0ad193411cd3509f57
imconfusedhelp <demskillzdough721129@gmail.com>
2023-01-30 14:50:35 -0800

    Update 1/30/23 2:50pm
    Made a script for LinearInterpolation

---
+++ ScriptingProjectOrtizP7/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+
+    // In this case, result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    // The Mathf.Lerp function takes 3 float parameters: 
+    // One representing the value to interpolate from; 
+    // Another representing the value to interpolate to 
+    // And a final float representing how far to interpolate
+
+    // 2nd example of Mathf.Lerp
+    Vector3 from = new Vector3(1f, 2f, 3);
+    Vector3 to = new Vector3(5f, 6f, 7f);
+
+    // Here results are going to be = (4, 5, 6)
+    Vector3 results = Vector3.Lerp(from, to, 0.75f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Light intensity increases towards 8 but the rate of which it happens slows down every FRAME
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+
+        // Light intensiy increases towards 8 but the rate of which it happens slows down every SECOND
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
